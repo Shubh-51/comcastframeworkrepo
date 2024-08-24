675b7b1b0ed23ffad575ef7a2f81c8b1f4a57062
shubh <shubh@Shubh_51.mshome.net>
2024-08-24 15:19:30 +0530

    Pom Class CreateNewProductPage added 1 element Element name ProductName

--- vtigermain/src/main/java/com/comcast/crm/objectrepositoryUtility/CreateNewProductPage.java
+++ vtigermain/src/main/java/com/comcast/crm/objectrepositoryUtility/CreateNewProductPage.java
@@ -1,5 +1,24 @@
 package com.comcast.crm.objectrepositoryUtility;
 
-public class CreateNewProductPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+public class CreateNewProductPage {
+	WebDriver driver=null;
+	
+	public CreateNewProductPage(WebDriver driver) {
+		this.driver=driver;
+		PageFactory.initElements(driver,this);
+	}
+	
+	@FindBy(name="discontinued")
+	private WebElement editCheckbox;
+
+	public WebElement getEditCheckbox() {
+		return editCheckbox;
+	}
+	
+	
 }
