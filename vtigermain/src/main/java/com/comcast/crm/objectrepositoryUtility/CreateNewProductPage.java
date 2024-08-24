9e8d982a0ac263eba5e63fad9e6bdc071df782ef
shubh <shubh@Shubh_51.mshome.net>
2024-08-24 14:37:59 +0530

    CreateNewProductPage is modified

--- vtigermain/src/main/java/com/comcast/crm/objectrepositoryUtility/CreateNewProductPage.java
+++ vtigermain/src/main/java/com/comcast/crm/objectrepositoryUtility/CreateNewProductPage.java
@@ -2,9 +2,4 @@ package com.comcast.crm.objectrepositoryUtility;
 
 public class CreateNewProductPage {
 	
-	public static void main(String[] args) {
-		System.out.println("shubh");
-		System.out.println("abv");
-	}
-    
 }
