df158a2f3d57bb35f8f6fe58c525d8f6355f2843
shubh <shubh@Shubh_51.mshome.net>
2024-08-24 13:31:43 +0530

    added new ProductPage class and CreateNewProductPage class

---
+++ vtigermain/src/main/java/com/comcast/crm/objectrepositoryUtility/ProductPage.java
@@ -0,0 +1,23 @@
+package com.comcast.crm.objectrepositoryUtility;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class ProductPage {
+	WebDriver driver=null;
+	public ProductPage(WebDriver driver) {
+		this.driver=driver;
+		PageFactory.initElements(driver, this);
+	}
+	
+	@FindBy(xpath="//img[@alt='Create Product...']")
+	private WebElement createNewProductBtn;
+	
+	
+	public WebElement getCreateNewProductBtn() {
+		return createNewProductBtn;
+	}
+	
+}
