ac2ae33071c36728c0129acbe9cabffb0ee59be2
shubh <shubh@Shubh_51>
2024-08-28 10:56:51 +0530

    @nd Push

---
+++ vtigermain/src/test/java/pac/timestamp/Capture2.java
@@ -0,0 +1,16 @@
+package pac.timestamp;
+
+import java.util.Date;
+
+public class Capture2 {
+	public static void main(String[] args) {
+		//String time=new Date().toString();
+		//System.out.println(time);
+		
+		//any folder name do not accept spaces and special characters
+		String time=new Date().toString().replace(" ", "_").replace(":", "_");
+		System.out.println(time);
+		System.out.println("hi");
+		System.out.println("hi");
+	}
+}
